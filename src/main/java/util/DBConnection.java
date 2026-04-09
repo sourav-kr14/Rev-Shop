223c780b480bebf6e713e22d7c33363cf1b5356a
souravkr14 <mca10021.24@bitmesra.ac.in>
2026-04-09 19:55:11 +0530

    update: sql jdbc connection done

--- src/main/java/util/DBConnection.java
+++ src/main/java/util/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 
 public class DBConnection
 {
-    private static final String URL="jdbc:mysql://localhost:3306/";
+    private static final String URL="jdbc:mysql://localhost:3306/revshop";
     private static final String USERNAME="root";
     private static final String PASSWORD="Singapore@145";
 
